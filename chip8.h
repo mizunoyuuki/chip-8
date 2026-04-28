4549749f2ba24f8913e6116bcb48cf1cc0bbc676
Ubuntu <ubuntu@ip-172-31-33-246.ap-northeast-1.compute.internal>
2026-04-28 17:17:47 +0000

    chip8のプロセッサ、メモリの構造体を実装、CPUの初期化実装

---
+++ chip8.h
@@ -0,0 +1,26 @@
+#ifndef CHIP8_H
+#define CHIP_H
+
+#include<stdint.h>
+
+typedef struct Chip8 Chip8;
+
+struct Chip8 {
+	uint8_t memory[4096];
+	uint8_t V[16];
+	uint16_t I;
+	uint16_t pc;
+
+	uint8_t delay_timer;
+	uint8_t sound_timer;
+
+	uint16_t stack[16];
+	uint8_t sp;
+
+	uint8_t keypad[16];
+	uint8_t display[64*32];
+};
+
+extern void chip8_init(Chip8*);
+
+#endif
