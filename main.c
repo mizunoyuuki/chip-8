2bd66b38c65317971a38ec0cfdec58272d3fdc09
Ubuntu <ubuntu@ip-172-31-33-246.ap-northeast-1.compute.internal>
2026-04-29 15:55:34 +0000

    SDLの初期化、仕組みを実装

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <SDL2/SDL.h>
 #include "chip8.h"
 
+#define SCALE 10
+
 Chip8 chip8;
 
 int main(int argc, char *argv[]){
@@ -17,6 +19,7 @@ int main(int argc, char *argv[]){
 
 	// CPU, メモリの初期化
 	chip8_init(&chip8);
+	srand(time(NULL));
 	
 	// ROM(プログラム)の読み込み
 	const char *rom_filename = argv[1];
@@ -27,15 +30,53 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	// メモリに読み込んだプログラムをエミュレートする
-	for (;;){
-		fetch();
-		decode();
-		execute();
-		store();
+	// SLD2初期化
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = SDL_CreateWindow("CHIP-8",
+			                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+					      64 * SCALE, 32 * SCALE, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Texture  *texture  = SDL_CreateTexture(renderer,
+			                           SDL_PIXELFORMAT_RGBA8888,
+						   SDL_TEXTUREACCESS_STREAMING,
+						   64, 32);
 
-		printf("V0: %d, V1: %d, x: %X, kk: 0x%02X\n", chip8.V[0], chip8.V[1], x, kk);
+	// メモリに読み込んだプログラムをエミュレートする
+	int running = 1;
+	while (running){
+		// イベント処理
+		SDL_Event event;
+		while(SDL_PollEvent(&event)){
+			if (event.type == SDL_QUIT) running = 0;
+		}
+
+		for (int i = 0; i < 10; i++){
+			fetch();
+			decode();
+			execute();
+			store();
+		}
+
+		// display[] -> テクスチャ -> 画面に描画
+		uint32_t pixels[64*32];
+		for (int i = 0; i < 64 *32; i++){
+			pixels[i] = chip8.display[i] ? 0xFFFFFFFF : 0x0000000F;
+		}
+		SDL_UpdateTexture(texture, NULL, pixels, 64 * sizeof(uint32_t));
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(16); // 約60fps
+	        
+		SDL_DestroyTexture(texture);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 0;
 	}
+
+	
 
 	return 0;
 }
